df66326514dbfbdef393e02baf491b523cb58c40
mguidoti <marcus.guidoti@gmail.com>
2020-10-30 20:45:54 -0300

    fear: Add storage functionality
    - install uuidv4
    - add storage in an array

--- src/index.js
+++ src/index.js
@@ -1,57 +1,78 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
+const projects = [];
+
 // HTML routes
 app.get('/projects', (request, response) => {
-    const query = request.query;
+    const { title } = request.query;
 
-    console.log(query);
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 5'
-    ]);
+    // console.log(query);
+
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-    const body = request.body;
+    const { title, owner } = request.body;
 
-    console.log(body);
+    const project = { id: uuid(), title, owner };
 
+    projects.push(project);
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-    const params = request.params;
+    // const params = request.params;
+    // const { id } = request.params;
+
+    // console.log(params);
+    // console.log(id);
+
     const { id } = request.params;
+    const { title, owner } = request.body;
 
-    console.log(params);
-    console.log(id);
+    const projectIndex = projects.findIndex(project => project.id === id);
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    if (projectIndex < 0) {
+        return response.status(400).json({error: 'project not found.'});
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    }
+
+    projects[projectIndex] = project;
+
+
+    return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({error: 'project not found.'});
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
 });
 
 // Listen to a specific port
 app.listen(3333, () => {
     console.log('Back-end started! ¯\\\_(ツ)_/¯')
-});
+}); 
