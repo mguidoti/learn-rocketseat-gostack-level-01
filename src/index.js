c3e2655363845a6596e49d620f84c3ed9e2b6455
mguidoti <marcus.guidoti@gmail.com>
2020-10-30 10:57:04 -0300

    feat: Add HTML methods routes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ const express = require('express');
 
 const app = express();
 
+// HTML routes
 app.get('/projects', (request, response) => {
-    return response.json({ 
-        message: 'Hello GoStack!!!'
-    });
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 5'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
 });
 
 // Listen to a specific port
