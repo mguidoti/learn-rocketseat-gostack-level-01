385410475d95b6445f3233c2e845d8025d56fa0a
mguidoti <marcus.guidoti@gmail.com>
2020-11-01 17:25:37 -0300

    chore: Setup babel on frontend workspace

---
+++ frontend/public/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var soma = function soma(a, b, c) {
+  return a + b + c;
+};
+
+console.log(soma(1, 2, 3));
---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+const soma = (a, b, c) => {
+    return a + b + c;
+}
+
+console.log(soma(1,2,3));
