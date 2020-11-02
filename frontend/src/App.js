917e72ae52a6fe987af950276f801d64f1b964bf
mguidoti <marcus.guidoti@gmail.com>
2020-11-02 10:18:47 -0300

    feat: Code for "Estado e imutabilidade" class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    function handleAddProject() {
+        // Old school JavaScript of doing things, not respecting the immutable
+        // concept required by React
+        //projects.push(`Novo projeto ${Date.now()}`);
+
+        // Applying the immutable concept
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+        console.log(projects);
+    }
+
     return (
         // Fragmentation concept, empty HTML tags to avoid unecessary additions
         // to the DOM
         <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <u>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </u>
-            </Header>
+            <Header title="Projects" />
+
+            <ul>
+                { projects.map(project => <li key={project}>{project}</li>) }
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
         </>
     );
 }
